6150733caac559b5695389c261524aad46194279
Mark Weinreuter <mark@weinreuter.org>
2019-05-10 14:16:11 +0200

    Versuche Pyside2 zu importieren, und erzwinge CLI Modus, falls PySide2 nicht verfügbar ist.

--- algo_battle/__main__.py
+++ algo_battle/__main__.py
@@ -2,7 +2,14 @@ import logging.config
 import sys
 
 from algo_battle.app.cli import run_cli
-from algo_battle.app.pyside2_gui import start_gui
+
+_gui_available = False
+try:
+    from algo_battle.app.pyside2_gui import start_gui
+
+    _gui_available = True
+except ImportError as e:
+    print("GUI Modus nicht verfügbar. Stelle sicher das du Pyside2 installiert hast.", file=sys.stderr)
 
 if __name__ == "__main__":
     logger = logging.getLogger("Main")
@@ -10,8 +17,12 @@ if __name__ == "__main__":
     mode = "CLI"
     modul_pfade = None
     if len(sys.argv) > 1:
-        mode = sys.argv[1]
+        mode = sys.argv[1].upper()
         modul_pfade = sys.argv[2:]
+
+    if not _gui_available and mode == "GUI":
+        print("GUI Modus nicht verfügbar. Starte CLI Modus.", file=sys.stderr)
+        mode = "CLI"
 
     if mode == "CLI":
         run_cli(modul_pfade)
